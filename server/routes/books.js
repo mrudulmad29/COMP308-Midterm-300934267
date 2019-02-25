85327bc4d64a09c5b80261e985f51638a2afb1ae
Mrudul Dubey <mrudulmad29@gmail.com>
2019-02-25 17:39:58 -0500

    first commit

---
+++ server/routes/books.js
@@ -0,0 +1,85 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let bookModel = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  bookModel.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/details', (req, res, next) => {
+
+  res.render('books/details', {
+    title: "Add a New Book"
+});
+
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/details', (req, res, next) => {
+
+  let newBook = bookModel({
+    "title":req.body.title,
+   // "description":req.body.DescriprionTextField,
+    "price":req.body.PriceTextField,
+    "author":req.body.AuthorTextField,
+    "genre":req.body.GenreTextField
+});
+
+bookModel.create(newBook, (err, books) => {
+    if(err) {
+        console.log(err);
+        res.end(err);
+    } 
+    else {
+        // takes the user back to the book-list page
+        res.redirect('/books');
+    }
+});
+
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+
+module.exports = router;
